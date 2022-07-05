e5e80ee93cd33554fb27066f4aa40e73d71117b1
qlxql <qlxqlrt2012@gmail.com>
2022-07-05 11:40:38 +0900

    1. 네비게이션 텝이 있는 앱 프로젝트 구조 설정 2. 홈 화면 상단 바 추가

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screen/home_screen.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Haaaaflix',
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+            accentColor: Colors.white
+        ),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                HomeScreen(),
+                Container(
+                  child: Center(
+                    child: Text(
+                        'search'),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: Text(
+                        'save'),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: Text(
+                        'more'),
+                  ),
+                ),
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          ),
+        )
+    );
+  }
+}
